58126399edfac73561b155574aa0e410b504f6ab
kichikuou <KichikuouChrome@gmail.com>
2023-10-14 09:29:55 +0900

    Set up OCamlFormat

--- bin/main.ml
+++ bin/main.ml
@@ -2,10 +2,12 @@ class lsp_server =
   object
     inherit Linol_lwt.Jsonrpc2.server
 
-    method on_notif_doc_did_open ~notify_back:_ _d ~content:_ : unit Linol_lwt.t =
+    method on_notif_doc_did_open ~notify_back:_ _d ~content:_ : unit Linol_lwt.t
+        =
       Linol_lwt.return ()
 
-    method on_notif_doc_did_change ~notify_back:_ _d _c ~old_content:_old ~new_content:_ =
+    method on_notif_doc_did_change ~notify_back:_ _d _c ~old_content:_old
+        ~new_content:_ =
       Linol_lwt.return ()
 
     method on_notif_doc_did_close ~notify_back:_ _d : unit Linol_lwt.t =
@@ -19,8 +21,8 @@ let run () =
   match Linol_lwt.run task with
   | () -> ()
   | exception e ->
-    let e = Printexc.to_string e in
-    Printf.eprintf "error: %s\n%!" e;
-    exit 1
+      let e = Printexc.to_string e in
+      Printf.eprintf "error: %s\n%!" e;
+      exit 1
 
 let () = run ()
