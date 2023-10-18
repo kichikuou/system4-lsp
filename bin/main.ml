08cda05831b5d7b91edf3f786f191060e06f7078
kichikuou <KichikuouChrome@gmail.com>
2023-10-18 10:57:00 +0900

    LSP: correctly calculate column offsets
    The character field of LSP's Position structre is UTF-16 based (LSP 3.17
    added support to negotiate the position encoding, but VSCode still only
    supports UTF-16).

--- bin/main.ml
+++ bin/main.ml
@@ -11,13 +11,31 @@ let log_info notify_back msg =
 let log_error notify_back msg =
   send_log_msg notify_back Lsp.Types.MessageType.Error msg
 
-let to_lsp_position p =
-  Lsp.Types.Position.create ~line:(p.Lexing.pos_lnum - 1)
-    ~character:(p.Lexing.pos_cnum - p.Lexing.pos_bol)
+(* LSP expects character offsets based on utf-16 representation. *)
+let count_utf16_code_units_of_utf8_bytes bytes start end_ =
+  let rec loop i n =
+    if i >= end_ then n
+    else
+      let ch = Bytes.get bytes i in
+      if Char.O.(ch <= '\x7f') then loop (i + 1) (n + 1)
+      else if Char.O.(ch <= '\xbf') then failwith "invalid utf-8 sequence"
+      else if Char.O.(ch <= '\xdf') then loop (i + 2) (n + 1)
+      else if Char.O.(ch <= '\xef') then loop (i + 3) (n + 1)
+      else if Char.O.(ch <= '\xf7') then loop (i + 4) (n + 2)
+      else failwith "invalid utf-8 sequence"
+  in
+  loop start 0
 
-let to_lsp_range (start, end_) =
-  Lsp.Types.Range.create ~start:(to_lsp_position start)
-    ~end_:(to_lsp_position end_)
+let to_lsp_position (lexbuf : Lexing.lexbuf) p =
+  Lsp.Types.Position.create ~line:(p.Lexing.pos_lnum - 1)
+    ~character:
+      (count_utf16_code_units_of_utf8_bytes lexbuf.lex_buffer p.Lexing.pos_bol
+         p.Lexing.pos_cnum)
+
+let to_lsp_range lexbuf (start, end_) =
+  Lsp.Types.Range.create
+    ~start:(to_lsp_position lexbuf start)
+    ~end_:(to_lsp_position lexbuf end_)
 
 let predefined_constants =
   [
@@ -79,21 +97,22 @@ class lsp_server =
         | Parser.Error ->
             let diag =
               Lsp.Types.Diagnostic.create
-                ~range:(to_lsp_range (lexbuf.lex_start_p, lexbuf.lex_curr_p))
+                ~range:
+                  (to_lsp_range lexbuf (lexbuf.lex_start_p, lexbuf.lex_curr_p))
                 ~message:"Syntax error." ()
             in
             [ diag ]
         | CompileError.CompileError (msg, node) ->
             let diag =
               Lsp.Types.Diagnostic.create
-                ~range:(Jaf.ast_node_pos node |> to_lsp_range)
+                ~range:(Jaf.ast_node_pos node |> to_lsp_range lexbuf)
                 ~message:msg ()
             in
             [ diag ]
         | CompileError.Undefined_variable (name, node) ->
             let diag =
               Lsp.Types.Diagnostic.create
-                ~range:(Jaf.ast_node_pos node |> to_lsp_range)
+                ~range:(Jaf.ast_node_pos node |> to_lsp_range lexbuf)
                 ~message:("Undefined variable: " ^ name)
                 ()
             in
